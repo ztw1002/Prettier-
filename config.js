8d74ac67950e7adb1c1474c4e7dca5b1d609065e
Zhu Tianwen <44706242+ztw1002@users.noreply.github.com>
2025-03-05 16:30:48 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,22 @@
+// .prettierrc.mjs
+
+export default {
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 100,
+  bracketSpacing: true,
+}
+
+
+// .prettierrc.js
+
+module.exports = {
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 100,
+  bracketSpacing: true,
+}
